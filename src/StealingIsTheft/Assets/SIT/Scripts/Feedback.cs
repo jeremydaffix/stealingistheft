977ef6f323fe7544277d8af9c972fa6bb0096fb2
rogerwilko <jdbasic@gmail.com>
2019-10-07 17:20:27 +0200

    messages

---
+++ src/StealingIsTheft/Assets/SIT/Scripts/Feedback.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Feedback : MonoBehaviour
+{
+    public static Feedback Instance = null;
+
+    [SerializeField]
+    private GameObject simpleMessagePrefab;
+
+
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+
+    public void ShowSimpleMessage(Vector2 pos, string txt, Color col, int size = 48, float duration = 0.25f, Transform p = null)
+    {
+        GameObject msg = Instantiate(simpleMessagePrefab, new Vector3(pos.x, pos.y, -5f), new Quaternion());
+
+        if(p != null)
+        {
+            msg.transform.parent = p;
+        }
+
+        TextMeshPro tmp = msg.GetComponent<TextMeshPro>();
+
+        if(tmp != null)
+        {
+            //tmp.text = txt;
+            tmp.color = col;
+            tmp.fontSize = size;
+
+            StartCoroutine(SimpleMessageAnim(tmp, txt, duration));
+        }
+    }
+
+
+    private IEnumerator SimpleMessageAnim(TextMeshPro tmp, string txt, float duration)
+    {
+        for(int i = 0; i < txt.Length; ++i)
+        {
+            tmp.text += txt[i];
+            yield return new WaitForSeconds(0.075f);
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        for(int j = 0; j < 20; ++j)
+        {
+            tmp.color = new Color(tmp.color.r, tmp.color.g, tmp.color.b, 1f - (j * 0.10f));
+
+            yield return new WaitForSeconds(0.025f);
+        }
+
+        Destroy(tmp.gameObject);
+    }
+}
---
+++ src/StealingIsTheft/Assets/SIT/Scripts/SimpleMessageOnWalk.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMessageOnWalk : MonoBehaviour
+{
+    [SerializeField]
+    private string message;
+
+    [SerializeField]
+    private Vector2 position = new Vector2();
+
+    [SerializeField]
+    private Color color = Color.white;
+
+    [SerializeField]
+    private int size = 48;
+
+    [SerializeField]
+    private float duration = 0.25f;
+
+    [SerializeField]
+    bool autoDestroy = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Debug.LogWarning(message);
+
+            Feedback.Instance.ShowSimpleMessage(position, message, color, size, duration);
+
+            if (autoDestroy)
+                Destroy(gameObject);
+        }
+    }
+
+}
